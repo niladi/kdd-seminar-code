f89c084518f8c0dc5ccd2e59467eca65fdac39a6
~Kris~ <kmdn@users.noreply.github.com>
2025-02-13 21:08:36 +0100

    Added command line argument reading

--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@ from clit_recommender.config import Config
 from clit_recommender.domain.datasets import Dataset
 from clit_recommender.domain.systems import System
 from clit_recommender.process.training import train_full
+import sys
 
 if __name__ == "__main__":
     freeze_support()
@@ -20,8 +21,24 @@ if __name__ == "__main__":
 
     _config.margin_range = 0.05
 
-    for i in range(2, 10):
-        _config.fixed_size_combo = i
+    #float(str(margin_arg))
+    # python
+    #sys.argv[0])
+    # fixed size
+    fixed_size = 2
+    if len(sys.argv) > 1:
+        fixed_size = int(sys.argv[1])
+    # margin
+    margin = 0.05
+    if len(sys.argv) > 2:
+        margin = float(sys.argv[2])
 
-        # _config.threshold = 0.3
-        train_full(_config, True, True)
+    #sys.argv[2])
+
+    _config.fixed_size_combo = fixed_size
+    _config.margin_range = margin
+
+    print(f"Running for fixed_size:   {_config.fixed_size_combo}")
+    print(f"Running for margin_range: {_config.margin_range}")
+    # _config.threshold = 0.3
+    train_full(_config, True, True)
